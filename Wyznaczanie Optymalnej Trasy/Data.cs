502a3b5ed5d56dbd9cfa8055f1911ae5d7887f65
AleksanderPawlak <aleksander.pawlak1996@gmail.com>
2019-10-26 19:35:36 +0200

    some basic structures with serialization

---
+++ Wyznaczanie Optymalnej Trasy/Data.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Google.Maps;
+
+using Wyznaczanie_Optymalnej_Trasy.Google;
+
+namespace Wyznaczanie_Optymalnej_Trasy
+{
+
+    //  Probably this structures will be changed
+    static class Globals
+    {
+        static bool AllowApi = false;
+        public const string API_KEY = "AIzaSyATIIYDzB6wVmdywhGVSmRLOWYrMHkrWBM";
+    }
+
+    class Data
+    {
+        // Data class should be initialized on program startup.
+        static string JsonFilesFolder = @".\";
+        static string CustomersJsonFilename = "Customers.json";
+        static string DimensionMatrixJsonFilename = "DimensionMatrix.json";
+        public List<Customer> CustomersList;
+        // public <jakis typ na macierz odleglosci> DimensionMatrix;
+
+        public Data()
+        {
+            //GoogleSigned.AssignAllServices(new GoogleSigned(Globals.API_KEY));
+            CustomersList = Deserialize<List<Customer>>(CustomersJsonFilename);
+        }
+
+        ~Data()
+        {
+            Serialize<List<Customer>>(CustomersJsonFilename, CustomersList);
+            //Serialize<>(DimensionMatrixJsonFilename, DimensionMatrix);
+        }
+
+        private T Deserialize<T>(string filename) where T : new()
+        {
+            try
+            {
+                using (System.IO.StreamReader stream = new System.IO.StreamReader(System.IO.Path.Combine(JsonFilesFolder, filename)))
+                {
+                    return JsonConvert.DeserializeObject<T>(stream.ReadToEnd());
+                }
+            }
+            catch (System.IO.FileNotFoundException)
+            {
+                return new T();
+            }
+        }
+
+        private void Serialize<T>(string filename, T data)
+        {
+            string jsonData = JsonConvert.SerializeObject(data);
+            System.IO.File.WriteAllText(System.IO.Path.Combine(JsonFilesFolder, filename), jsonData);
+        }
+
+        public void AddCustomer(
+            string name, decimal latitude, decimal longitude, string street, int buildingNumber, 
+            int houseNumber, string zipcode, string city, string country
+            )
+        { 
+            
+        }
+    }
+}
---
+++ Wyznaczanie Optymalnej Trasy/Structures.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Google.Maps;
+
+namespace Wyznaczanie_Optymalnej_Trasy.Google
+{
+    public class Address  // Maybe Google.maps have structures for that
+    {
+        //public PointLatLng Coordinates;
+        public LatLng Coordinates { get; set; } = default;
+        public string Street;
+        public int BuildingNumber;
+        public int HouseNumber;
+        public string ZipCode;
+        public string City;
+        public string Country;
+
+        public Location AsLocation()
+        {
+            if (!Object.Equals(Coordinates, default))
+            {
+                return new Location(Coordinates.ToString());
+            }
+            else
+            {
+                string locationString = Street + BuildingNumber.ToString() + ZipCode + City;
+                return new Location(locationString);
+            }
+        }
+    }
+
+     public class Customer
+    {
+        public string Name;
+        public Address Address;   
+
+
+    }
+
+}
