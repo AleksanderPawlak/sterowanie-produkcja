3ab0e9b948d38e397c09d551e7483dc73fa35b90
AleksanderPawlak <aleksander.pawlak1996@gmail.com>
2019-10-27 12:37:13 +0100

    Fixed serialization and deserialization of Customer class

--- Wyznaczanie Optymalnej Trasy/Form1.Designer.cs
+++ Wyznaczanie Optymalnej Trasy/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.CustomersList = new System.Windows.Forms.TabPage();
+            this.DimensionsRefreshButton = new System.Windows.Forms.Button();
             this.AddCustomerButton = new System.Windows.Forms.Button();
             this.CustomerListview = new System.Windows.Forms.ListView();
             this.name = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
@@ -41,8 +42,18 @@
             this.zipcode = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.city = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.Algorithm = new System.Windows.Forms.TabPage();
+            this.CustomersListCheck = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader8 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.tabControl1.SuspendLayout();
             this.CustomersList.SuspendLayout();
+            this.Algorithm.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -57,6 +68,7 @@
             // 
             // CustomersList
             // 
+            this.CustomersList.Controls.Add(this.DimensionsRefreshButton);
             this.CustomersList.Controls.Add(this.AddCustomerButton);
             this.CustomersList.Controls.Add(this.CustomerListview);
             this.CustomersList.Location = new System.Drawing.Point(4, 40);
@@ -67,9 +79,18 @@
             this.CustomersList.Text = "Lista odbiorców";
             this.CustomersList.UseVisualStyleBackColor = true;
             // 
+            // DimensionsRefreshButton
+            // 
+            this.DimensionsRefreshButton.Location = new System.Drawing.Point(266, 950);
+            this.DimensionsRefreshButton.Name = "DimensionsRefreshButton";
+            this.DimensionsRefreshButton.Size = new System.Drawing.Size(270, 104);
+            this.DimensionsRefreshButton.TabIndex = 2;
+            this.DimensionsRefreshButton.Text = "Przelicz odległości";
+            this.DimensionsRefreshButton.UseVisualStyleBackColor = true;
+            // 
             // AddCustomerButton
             // 
-            this.AddCustomerButton.Location = new System.Drawing.Point(7, 951);
+            this.AddCustomerButton.Location = new System.Drawing.Point(6, 950);
             this.AddCustomerButton.Name = "AddCustomerButton";
             this.AddCustomerButton.Size = new System.Drawing.Size(254, 104);
             this.AddCustomerButton.TabIndex = 1;
@@ -138,6 +159,7 @@
             // 
             // Algorithm
             // 
+            this.Algorithm.Controls.Add(this.CustomersListCheck);
             this.Algorithm.Location = new System.Drawing.Point(4, 40);
             this.Algorithm.Name = "Algorithm";
             this.Algorithm.Padding = new System.Windows.Forms.Padding(3);
@@ -145,6 +167,67 @@
             this.Algorithm.TabIndex = 1;
             this.Algorithm.Text = "Wyznacz trasę";
             this.Algorithm.UseVisualStyleBackColor = true;
+            // 
+            // CustomersListCheck
+            // 
+            this.CustomersListCheck.CheckBoxes = true;
+            this.CustomersListCheck.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6,
+            this.columnHeader7,
+            this.columnHeader8});
+            this.CustomersListCheck.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.CustomersListCheck.HideSelection = false;
+            this.CustomersListCheck.Location = new System.Drawing.Point(6, 0);
+            this.CustomersListCheck.Name = "CustomersListCheck";
+            this.CustomersListCheck.Size = new System.Drawing.Size(1504, 941);
+            this.CustomersListCheck.TabIndex = 1;
+            this.CustomersListCheck.UseCompatibleStateImageBehavior = false;
+            this.CustomersListCheck.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Nazwa";
+            this.columnHeader1.Width = 164;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "szerokość geogr.";
+            this.columnHeader2.Width = 221;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "długość geogr.";
+            this.columnHeader3.Width = 215;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "ulica";
+            this.columnHeader4.Width = 98;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "nr budynku";
+            this.columnHeader5.Width = 164;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "nr lokalu";
+            this.columnHeader6.Width = 162;
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = "kod pocztowy";
+            this.columnHeader7.Width = 186;
+            // 
+            // columnHeader8
+            // 
+            this.columnHeader8.Text = "miasto";
+            this.columnHeader8.Width = 267;
             // 
             // Form1
             // 
@@ -156,6 +239,7 @@
             this.Text = "Wyznaczanie optymalnej trasy";
             this.tabControl1.ResumeLayout(false);
             this.CustomersList.ResumeLayout(false);
+            this.Algorithm.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -175,6 +259,16 @@
         private System.Windows.Forms.ColumnHeader zipcode;
         private System.Windows.Forms.ColumnHeader city;
         private System.Windows.Forms.Button AddCustomerButton;
+        private System.Windows.Forms.Button DimensionsRefreshButton;
+        private System.Windows.Forms.ListView CustomersListCheck;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.ColumnHeader columnHeader7;
+        private System.Windows.Forms.ColumnHeader columnHeader8;
     }
 }
 
--- Wyznaczanie Optymalnej Trasy/Structures.cs
+++ Wyznaczanie Optymalnej Trasy/Structures.cs
@@ -7,22 +7,40 @@ using Google.Maps;
 
 namespace Wyznaczanie_Optymalnej_Trasy
 {
+    [Serializable]
+    public struct Coordinates
+    {
+        public decimal Latitude;
+        public decimal Longitude;
+    
+        public Coordinates(decimal latitude, decimal longitude)
+        {
+            Latitude = latitude;
+            Longitude = longitude;
+        }
+    }
+
+
+    [Serializable]
      public class Customer
     {
-        public string Name;
-        // Maybe Google.maps have structures for that
-        public LatLng Coordinates { get; set; } = default;
-        public string Street { get; set; } = default;
-        public int BuildingNumber { get; set; } = default;
-        public int HouseNumber { get; set; } = default;
-        public string ZipCode { get; set; } = default;
-        public string City { get; set; } = default;
+        public string Name = default;
+        public Coordinates Coordinates = default;
+        public string Street = default;
+        public int BuildingNumber = default;
+        public int HouseNumber = default;
+        public string ZipCode = default;
+        public string City = default;
         public string Country = "PL"; // TODO: check if google api accepts country and adjust
+
+        public Customer()
+        { 
+        }
 
         public Customer(string name, decimal latitude, decimal longitude)
         {
             Name = name;
-            Coordinates = new LatLng(latitude, longitude);
+            Coordinates = new Coordinates(latitude, longitude);
         }
         public Customer(
             string name, decimal latitude, decimal longitude, string street, int buildingNumber,
@@ -30,7 +48,7 @@ namespace Wyznaczanie_Optymalnej_Trasy
             )
         {
             Name = name;
-            Coordinates = new LatLng(latitude, longitude);
+            Coordinates = new Coordinates(latitude, longitude);
             Street = street;
             BuildingNumber = buildingNumber;
             HouseNumber = houseNumber;
