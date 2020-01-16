27f61eaa49a2d1f58dbaabf3e56b6270bb25d6e1
maciej.filinski <maciej.filinski@gmail.com>
2020-01-16 14:51:34 +0100

    Wstępne zmiany w algorytmie

---
+++ Wyznaczanie Optymalnej Trasy/Algorithms/utils/SA_Result.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Simulated_annealing;
+
+namespace Wyznaczanie_Optymalnej_Trasy.Algorithms.utils
+{
+    class SA_Result
+    {
+        public Sequence optimal_road;
+        public List<int> ReturnCity;
+
+        public SA_Result() { }
+    }
+}
